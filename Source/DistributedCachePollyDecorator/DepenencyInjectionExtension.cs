38a067baba8f269df1651c837027b18305a94f75
darthfabar <darth-fabar@posteo.de>
2021-03-26 13:11:08 +0100

    REFACTOR policy injection and policy builder

--- Source/DistributedCachePollyDecorator/DepenencyInjectionExtension.cs
+++ Source/DistributedCachePollyDecorator/DepenencyInjectionExtension.cs
@@ -9,22 +9,6 @@ namespace DistributedCachePollyDecorator.Policies
     /// </summary>
     public static class DepenencyInjectionExtension
     {
-        /// <summary>
-        /// Creates a new PolicyRegistry and adds CircuitBreaker to it.
-        /// </summary>
-        /// <typeparam name="TCircuitBreakerSettings">Settingstype.</typeparam>
-        /// <param name="services">Extension of services.</param>
-        /// <param name="breakerSettings">Settings.</param>
-        /// <returns>ÌServiceCollection.</returns>
-        [Discardable]
-        public static IServiceCollection AddPolicyForDistributedCache<TCircuitBreakerSettings>(this IServiceCollection services, TCircuitBreakerSettings breakerSettings)
-            where TCircuitBreakerSettings : CircuitBreakerSettingsBase, new()
-        {
-            services
-                .AddPolicyRegistry()
-                .AddPoliciesToRegistry(breakerSettings);
-            return services;
-        }
 
         /// <summary>
         /// Add the IDistributedCache decorator to DI and sets up the Policies for it.
@@ -43,18 +27,78 @@ namespace DistributedCachePollyDecorator.Policies
         }
 
         /// <summary>
-        /// Adds Policies to a PolicyRegistry.
+        /// Add CacheDecorator with your own Polly Policies
+        /// </summary>
+        /// <typeparam name="TAsyncPolicy"></typeparam>
+        /// <typeparam name="TSyncPolicy"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="syncPolicy"></param>
+        /// <param name="asyncPolicy"></param>
+        /// <returns></returns>
+        [Discardable]
+        public static IServiceCollection AddDistributedCacheDecorator<TAsyncPolicy, TSyncPolicy>(this IServiceCollection services, TSyncPolicy syncPolicy, TAsyncPolicy asyncPolicy)
+            where TAsyncPolicy : Polly.IAsyncPolicy
+            where TSyncPolicy : Polly.ISyncPolicy
+        {
+            return services
+                .AddPolicyForDistributedCache(syncPolicy, asyncPolicy)
+                .Decorate<IDistributedCache, PolicyDistributedCacheDecorator>();
+        }
+
+        /// <summary>
+        /// Creates a new PolicyRegistry and adds CircuitBreaker to it.
         /// </summary>
         /// <typeparam name="TCircuitBreakerSettings">Settingstype.</typeparam>
-        /// <param name="registry">PolicyRegistry to use.</param>
+        /// <param name="services">Extension of services.</param>
         /// <param name="breakerSettings">Settings.</param>
-        /// <returns>same PolicyRegistry instance as before.</returns>
+        /// <returns>ÌServiceCollection.</returns>
         [Discardable]
-        private static Polly.Registry.IPolicyRegistry<string> AddPoliciesToRegistry<TCircuitBreakerSettings>(this Polly.Registry.IPolicyRegistry<string> registry, TCircuitBreakerSettings breakerSettings)
+        public static IServiceCollection AddPolicyForDistributedCache<TCircuitBreakerSettings>(this IServiceCollection services, TCircuitBreakerSettings breakerSettings)
             where TCircuitBreakerSettings : CircuitBreakerSettingsBase, new()
         {
-            registry.Add(PollyPolicies.AsyncPolicy, PollyPolicies.GetAsyncCircuitBreaker(breakerSettings));
-            registry.Add(PollyPolicies.SyncPolicy, PollyPolicies.GetSyncCircuitBreaker(breakerSettings));
+            services
+                .AddPolicyRegistry()
+                .AddPoliciesToRegistry(PollyPolicies.GetSyncCircuitBreaker(breakerSettings), PollyPolicies.GetAsyncCircuitBreaker(breakerSettings));
+            return services;
+        }
+
+
+        /// <summary>
+        /// Register your own policies directly
+        /// </summary>
+        /// <typeparam name="TAsyncPolicy"></typeparam>
+        /// <typeparam name="TSyncPolicy"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="syncPolicy"></param>
+        /// <param name="asyncPolicy"></param>
+        /// <returns></returns>
+        [Discardable]
+        public static IServiceCollection AddPolicyForDistributedCache<TAsyncPolicy, TSyncPolicy>(this IServiceCollection services, TSyncPolicy syncPolicy, TAsyncPolicy asyncPolicy)
+            where TAsyncPolicy : Polly.IAsyncPolicy
+            where TSyncPolicy : Polly.ISyncPolicy
+        {
+            services
+                .AddPolicyRegistry()
+                .AddPoliciesToRegistry(syncPolicy, asyncPolicy);
+            return services;
+        }
+
+        /// <summary>
+        /// Adds Policies to a PolicyRegistry.
+        /// </summary>
+        /// <typeparam name="TAsyncPolicy"></typeparam>
+        /// <typeparam name="TSyncPolicy"></typeparam>
+        /// <param name="registry"></param>
+        /// <param name="syncPolicy"></param>
+        /// <param name="asyncPolicy"></param>
+        /// <returns></returns>
+        [Discardable]
+        private static Polly.Registry.IPolicyRegistry<string> AddPoliciesToRegistry<TAsyncPolicy, TSyncPolicy>(this Polly.Registry.IPolicyRegistry<string> registry, TSyncPolicy syncPolicy, TAsyncPolicy asyncPolicy)
+            where TAsyncPolicy : Polly.IAsyncPolicy
+            where TSyncPolicy : Polly.ISyncPolicy
+        {
+            registry.Add(PollyPolicies.AsyncPolicy, asyncPolicy);
+            registry.Add(PollyPolicies.SyncPolicy, syncPolicy);
             return registry;
         }
     }
